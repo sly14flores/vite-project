3b030caddd0a0d17428708097a42f39ecea30e0b
Sly Flores <sly@christian.com.ph>
2022-11-01 10:34:08 +0800

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,12 @@ const App = () => {
         navigation={navigation}
         sidebarOpen={sidebarOpen}
         setSidebarOpen={setSidebarOpen}
-      />
+        title='Dashboard'
+      >
+        <div className="py-4">
+            <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+        </div>
+      </Wrapper>
     </>
   )
 }
--- src/components/Wrapper/index.jsx
+++ src/components/Wrapper/index.jsx
@@ -14,6 +14,8 @@ const Wrapper = ({
     navigation,
     sidebarOpen,
     setSidebarOpen,
+    title,
+    children,
 }) => {
 
     return (
@@ -27,88 +29,89 @@ const Wrapper = ({
                 ```
             */}
             <div>
-                <Transition.Root show={sidebarOpen} as={Fragment}>
-                <Dialog as="div" className="relative z-40 md:hidden" onClose={setSidebarOpen}>
-                    <Transition.Child
-                    as={Fragment}
-                    enter="transition-opacity ease-linear duration-300"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="transition-opacity ease-linear duration-300"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                    >
-                    <div className="fixed inset-0 bg-gray-600 bg-opacity-75" />
-                    </Transition.Child>
 
-                    <div className="fixed inset-0 z-40 flex">
-                    <Transition.Child
+                <Transition.Root show={sidebarOpen} as={Fragment}>
+                    <Dialog as="div" className="relative z-40 md:hidden" onClose={setSidebarOpen}>
+                        <Transition.Child
                         as={Fragment}
-                        enter="transition ease-in-out duration-300 transform"
-                        enterFrom="-translate-x-full"
-                        enterTo="translate-x-0"
-                        leave="transition ease-in-out duration-300 transform"
-                        leaveFrom="translate-x-0"
-                        leaveTo="-translate-x-full"
-                    >
-                        <Dialog.Panel className="relative flex w-full max-w-xs flex-1 flex-col bg-gray-800">
+                        enter="transition-opacity ease-linear duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="transition-opacity ease-linear duration-300"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                        >
+                        <div className="fixed inset-0 bg-gray-600 bg-opacity-75" />
+                        </Transition.Child>
+
+                        <div className="fixed inset-0 z-40 flex">
                         <Transition.Child
                             as={Fragment}
-                            enter="ease-in-out duration-300"
-                            enterFrom="opacity-0"
-                            enterTo="opacity-100"
-                            leave="ease-in-out duration-300"
-                            leaveFrom="opacity-100"
-                            leaveTo="opacity-0"
+                            enter="transition ease-in-out duration-300 transform"
+                            enterFrom="-translate-x-full"
+                            enterTo="translate-x-0"
+                            leave="transition ease-in-out duration-300 transform"
+                            leaveFrom="translate-x-0"
+                            leaveTo="-translate-x-full"
                         >
-                            <div className="absolute top-0 right-0 -mr-12 pt-2">
-                            <button
-                                type="button"
-                                className="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                                onClick={() => setSidebarOpen(false)}
+                            <Dialog.Panel className="relative flex w-full max-w-xs flex-1 flex-col bg-gray-800">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-in-out duration-300"
+                                enterFrom="opacity-0"
+                                enterTo="opacity-100"
+                                leave="ease-in-out duration-300"
+                                leaveFrom="opacity-100"
+                                leaveTo="opacity-0"
                             >
-                                <span className="sr-only">Close sidebar</span>
-                                <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
-                            </button>
-                            </div>
-                        </Transition.Child>
-                        <div className="h-0 flex-1 overflow-y-auto pt-5 pb-4">
-                            <div className="flex flex-shrink-0 items-center px-4">
-                            <img
-                                className="h-8 w-auto"
-                                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                                alt="Your Company"
-                            />
-                            </div>
-                            <nav className="mt-5 space-y-1 px-2">
-                            {navigation.map((item) => (
-                                <a
-                                key={item.name}
-                                href={item.href}
-                                className={classNames(
-                                    item.current
-                                    ? 'bg-gray-900 text-white'
-                                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                    'group flex items-center px-2 py-2 text-base font-medium rounded-md'
-                                )}
+                                <div className="absolute top-0 right-0 -mr-12 pt-2">
+                                <button
+                                    type="button"
+                                    className="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                                    onClick={() => setSidebarOpen(false)}
                                 >
-                                <item.icon
-                                    className={classNames(
-                                    item.current ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300',
-                                    'mr-4 flex-shrink-0 h-6 w-6'
-                                    )}
-                                    aria-hidden="true"
+                                    <span className="sr-only">Close sidebar</span>
+                                    <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                                </button>
+                                </div>
+                            </Transition.Child>
+                            <div className="h-0 flex-1 overflow-y-auto pt-5 pb-4">
+                                <div className="flex flex-shrink-0 items-center px-4">
+                                <img
+                                    className="h-8 w-auto"
+                                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                                    alt="Your Company"
                                 />
-                                {item.name}
-                                </a>
-                            ))}
-                            </nav>
+                                </div>
+                                <nav className="mt-5 space-y-1 px-2">
+                                    {navigation.map((item) => (
+                                        <a
+                                        key={item.name}
+                                        href={item.href}
+                                        className={classNames(
+                                            item.current
+                                            ? 'bg-gray-900 text-white'
+                                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                            'group flex items-center px-2 py-2 text-base font-medium rounded-md'
+                                        )}
+                                        >
+                                        <item.icon
+                                            className={classNames(
+                                            item.current ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300',
+                                            'mr-4 flex-shrink-0 h-6 w-6'
+                                            )}
+                                            aria-hidden="true"
+                                        />
+                                        {item.name}
+                                        </a>
+                                    ))}
+                                </nav>
+                            </div>
+                            </Dialog.Panel>
+                        </Transition.Child>
+                        <div className="w-14 flex-shrink-0">{/* Force sidebar to shrink to fit close icon */}</div>
                         </div>
-                        </Dialog.Panel>
-                    </Transition.Child>
-                    <div className="w-14 flex-shrink-0">{/* Force sidebar to shrink to fit close icon */}</div>
-                    </div>
-                </Dialog>
+                    </Dialog>
                 </Transition.Root>
 
                 {/* Static sidebar for desktop */}
@@ -147,6 +150,7 @@ const Wrapper = ({
                     </div>
                 </div>
                 </div>
+
                 <div className="flex flex-1 flex-col md:pl-64">
                 <div className="sticky top-0 z-10 bg-gray-100 pl-1 pt-1 sm:pl-3 sm:pt-3 md:hidden">
                     <button
@@ -159,20 +163,14 @@ const Wrapper = ({
                     </button>
                 </div>
                 <main className="flex-1">
-
                     <div className="py-6">
-                    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                        <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
-                    </div>
-                    <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-                        {/* Replace with your content */}
-                        <div className="py-4">
-                        <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+                        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                            <h1 className="text-2xl font-semibold text-gray-900">{title}</h1>
                         </div>
-                        {/* /End replace */}
+                        <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
+                            {children}
+                        </div>
                     </div>
-                    </div>
-
                 </main>
                 </div>
             </div>
